1962d4f5d1aae462a11e0730400e4d8a2cc04521
BuildTools <unconfigured@null.spigotmc.org>
2020-12-16 14:00:28 +0100

    Zu2weit - 1.16.4

--- src/CommandExecutor/CMD_Stop.java
+++ src/CommandExecutor/CMD_Stop.java
@@ -15,7 +15,6 @@ import ru.tehkode.permissions.bukkit.PermissionsEx;
 
 public class CMD_Stop implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        Player p = (Player) sender;
         if (sender instanceof ConsoleCommandSender) {
             if (cmd.getName().equalsIgnoreCase("stop")) {
                 for (int i = 3; i > 0; i--) {
@@ -32,11 +31,11 @@ public class CMD_Stop implements CommandExecutor {
                 }
                 Bukkit.shutdown();
             }
-        } else if (PermissionsEx.getUser(p).inGroup("Owner") || PermissionsEx.getUser(p).inGroup("Vice")) {
-            Bukkit.getConsoleSender().sendMessage("§4! §e" + p.getPlayer().getName() + " tried to stop the server");
-            p.sendMessage("§7| §4Only for the §bConsoleCommandSender");
+        } else if (PermissionsEx.getUser((Player)sender).inGroup("Owner") || PermissionsEx.getUser((Player)sender).inGroup("Vice")) {
+            Bukkit.getConsoleSender().sendMessage("§4! §e" + sender.getName() + " tried to stop the server");
+            sender.sendMessage("§7| §4Only for the §bConsoleCommandSender");
         } else {
-            p.sendMessage(main.noperm);
+            sender.sendMessage(main.noperm);
         }
         return false;
     }
